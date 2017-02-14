d3ea5fc7b2bf8aaad5f740b8e25b56f74cc0d179
Devin Cornell <devinj.cornell@gmail.com>
2017-02-13 22:58:51 -0800

    Worked on build_network, but still having some problems with the combination of cofrequency across articles..

--- build_network.py
+++ build_network.py
@@ -5,56 +5,115 @@ from nltk.corpus import stopwords
 import networkx as nx
 from multiprocessing import Pool
 
+parallel = False
 
-en_stop = set(stopwords.words('english'))
-tokenizer = RegexpTokenizer(r'\w+')
-
-with open('Data/scraped_articles_cbsnews.json', 'r') as f:
-	dat = json.load(f)
-
-allp = list()
-for url, dat in dat.items():
-	yr = str(dat['metadata']['timestamp'])[0:4]
-	mo = str(dat['metadata']['timestamp'])[4:6]
-	pars = dat['story_content'].split('\u00a0')
-	
-	#print(len(pars))
-	allp = allp + pars
-
-	#exit()
-
-#allp = allp
+flist = [
+	'Data/scraped_articles_breitbart.json',
+	'Data/scraped_articles_cbsnews.json',
+	'Data/scraped_articles_cnn.json',
+	'Data/scraped_articles_foxnews.json',
+	'Data/scraped_articles_nytimes.json',
+	]
 
 
-pars = []
-for i in range(len(allp)):
+# map function to get cofrequency given a paragraph of text
+def get_cofreq(allp):
+	# tokenize paragraphs
+	pars = list()
+	for i in range(len(allp)):
+		allp[i] = allp[i]\
+			.replace('\u2019','')\
+			.replace('\u2014','')\
+			.replace('\u201c','')\
+			.replace('\u201d','')\
+			.replace('\u2018','')
 
-	allp[i] = allp[i]\
-		.replace('\u2019','')\
-		.replace('\u2014','')\
-		.replace('\u201c','')\
-		.replace('\u201d','')\
-		.replace('\u2018','')
-	
-	tokens = tokenizer.tokenize(allp[i].lower())
-	tokens = set([i for i in tokens if not i in en_stop])
-	#print(len(tokens))
+		# paragraphs as a set of tokens
+		tokens = tokenizer.tokenize(allp[i].lower())
+		tokens = set([i for i in tokens if not i in en_stop])
 	pars.append(tokens)
+
+	# get co-occurrence frequencies
+	cofreq = {(a,b):0 for p in pars for a in p for b in p}
+	for p in pars:
+		for a in p:
+			for b in p:
+				cofreq[(a,b)] += 1
 	
-	#print('=====')
-	#print(allp[i])
-
-G = nx.Graph()
-for p in pars:
-	for a in p:
-		for b in p:
-			try:
-				G.edge[a][b]
-			except:
-				G.add_edge(a,b,weight=0)
-			G.edge[a][b]['weight'] += 1
-
-for e in G.edges()
 
 
-nx.write_gexf(G,'test.gexf')
+	# This will be done in the earlier...
+	# remove zeros and reciprocols
+	#cofreq = {k:v for k,v in cofreq.items() if v > 0}
+	#okeys = set(cofreq.keys())
+	#for k in okeys:
+	#	if (k[1],k[0]) in cofreq.keys():
+	#		cofreq[k] += cofreq[(k[1],k[0])]
+	#		del cofreq[(k[1],k[0])]
+
+	return cofreq
+
+
+if __name__ == '__main__':
+	# set up tokenizer and stop words
+	en_stop = set(stopwords.words('english'))
+	tokenizer = RegexpTokenizer(r'\w+')
+
+	# loop through each article of each source
+	articles = list() # list of all paragraphs
+	for fname in flist:
+		print('Loading data file {}.'.format(fname))
+		with open(fname, 'r') as f:
+			dat = json.load(f)
+
+		for url, dat in dat.items():
+			# get metadata
+			yr = str(dat['metadata']['timestamp'])[0:4]
+			mo = str(dat['metadata']['timestamp'])[4:6]
+			
+			# split into paragraphs
+			pars = dat['story_content'].split('\u00a0')
+			articles.append(pars)
+
+	articles = articles[0:10]
+
+	# map paragraph strings to cofrequency counts
+	if parallel:
+		print('Applying parallel map..')
+		p = Pool(10)
+		art_cofreq = list(p.map(get_cofreq, articles))
+	else:
+		print('Applying map..')
+		art_cofreq = list(map(get_cofreq, articles))
+
+	# combine cofreq from all articles
+	print('Combining cofreq counts from articles..')
+	cofreq = dict()
+	for art in art_cofreq:
+		for k in art.keys():
+			if k in cofreq.keys():
+				cofreq[k] += art[k]
+			else:
+				cofreq[k] = art[k]
+
+	# construct graph
+	print('Constructing Graph..')
+	G = nx.Graph()
+	for k in cofreq.keys():
+		#if (k[0],k[1]) not in G.edges():
+		try:
+			G.edge[k[0]][k[1]]
+		except:
+			G.add_edge(k[0],k[1],count=0)
+		G.edge[k[0]][k[1]]['count'] += 1
+
+	# apply edge attribute weight (inverse of count)
+	weights = {(e[0],e[1]):1/e[2]['count'] for e in G.edges(data=True)}
+	nx.set_edge_attributes(G,'weight', weights)
+
+	# save file
+	nx.write_gexf(G,'test.gexf')
+	print('Saved network .gexf file.')
+
+
+
