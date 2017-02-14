f1b7fbe8d1bf2146ac79b9706b96cd16ab74282d
Devin <devinj.cornell@gmail.com>
2017-02-13 20:44:29 -0800

    Finished weird initial graph.

---
+++ datainfo.py
@@ -0,0 +1,53 @@
+
+import json
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+import networkx as nx
+
+en_stop = set(stopwords.words('english'))
+tokenizer = RegexpTokenizer(r'\w+')
+
+with open('Data/scraped_articles_cbsnews.json', 'r') as f:
+	dat = json.load(f)
+
+allp = list()
+for url, dat in dat.items():
+	yr = str(dat['metadata']['timestamp'])[0:4]
+	mo = str(dat['metadata']['timestamp'])[4:6]
+	pars = dat['story_content'].split('\u00a0')
+	
+	#print(len(pars))
+	allp = allp + pars
+
+	#exit()
+
+allp = allp[0:3]
+
+pars = []
+for i in range(len(allp)):
+
+	allp[i] = allp[i]\
+		.replace('\u2019','')\
+		.replace('\u2014','')\
+		.replace('\u201c','')\
+		.replace('\u201d','')\
+		.replace('\u2018','')
+	
+	tokens = tokenizer.tokenize(allp[i].lower())
+	tokens = set([i for i in tokens if not i in en_stop])
+	pars.append(tokens)
+	
+	#print('=====')
+	#print(allp[i])
+
+G = nx.Graph()
+for p in pars:
+	for a in p:
+		for b in p:
+			try:
+				G.edge[a][b]
+			except:
+				G.add_edge(a,b,weight=0)
+			G.edge[a][b]['weight'] += 1
+
+nx.write_gexf(G,'test.gexf')
