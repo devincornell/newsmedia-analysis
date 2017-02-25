4cf62595383e24b4a9c48d359af4542c43c4a6c7
Devin Cornell <devinj.cornell@gmail.com>
2017-02-25 14:51:10 -0800

    Added script to build the w2v articles for each source.

---
+++ build_relational_network.py
@@ -0,0 +1,46 @@
+
+import gensim.models
+import json
+import nltk
+
+def get_source_data(fname,verbose=False):
+    if verbose: print('Loading data file {}.'.format(fname))
+    with open(fname,encoding='utf-8', errors='surrogateescape') as f:
+        filestr = f.read()
+        dat = json.loads(filestr)
+
+    for url, data in dat.items():
+        # get metadata
+        data['metadata']['yr'] = str(data['metadata']['timestamp'])[0:4]
+        data['metadata']['mo'] = str(data['metadata']['timestamp'])[4:6]
+        data['story_content'] = data['story_content'].encode('utf-8')
+
+    return dat
+
+
+if __name__ == "__main__":
+
+    # script params
+    outfile = "results/model.idk"
+
+    sources = {
+        'breitbart': 'Data/scraped_articles_breitbart.json',
+        'cbsnews': 'Data/scraped_articles_cbsnews.json',
+        'cnn': 'Data/scraped_articles_cnn.json',
+        'foxnews': 'Data/scraped_articles_foxnews.json',
+        'nytimes': 'Data/scraped_articles_nytimes.json',
+        }
+
+    for srcname, srcfile in sources.items():
+        articles = get_source_data(srcfile)
+        src_sent = list()
+        for url, art in articles.items():
+            story = art['story_content']
+            sent_tokens = nltk.sent_tokenize(str(story))
+            src_sent = src_sent + sent_tokens
+
+        # train model on all sentences from source
+        print("Training model on {}".format(srcname))
+        model = gensim.models.Word2Vec(sent_tokens, size=20,workers=6)
+        model.save('srcname.wtvmodel')
+
