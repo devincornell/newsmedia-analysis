c327c716dafcfac53e60f4aed6cbdd5ce4eec815
Devin Cornell <devinj.cornell@gmail.com>
2017-02-25 15:24:02 -0800

    Fixed build_models.py to remove some special characters in the articles prior to processing.

--- build_models.py
+++ build_models.py
@@ -3,6 +3,13 @@ import gensim.models
 import json
 import nltk
 
+def remove_specialchars(tstr):
+    return tstr\
+        .replace(u'\\','')\
+        .replace(u'\\n','')\
+        .replace(u"'",'')\
+        .replace(u'"','')
+
 def get_source_data(fname,verbose=False):
     if verbose: print('Loading data file {}.'.format(fname))
     with open(fname,encoding='utf-8', errors='surrogateescape') as f:
@@ -37,10 +44,13 @@ if __name__ == "__main__":
         for url, art in articles.items():
             story = art['story_content']
             sent_tokens = nltk.sent_tokenize(str(story))
+            sent_tokens = [str(remove_specialchars(sent)) for sent in sent_tokens]
+
             src_sent = src_sent + sent_tokens
 
         # train model on all sentences from source
+        print('{} sentences for {}.'.format(len(src_sent), srcname))
         print("Training model on {}".format(srcname))
         model = gensim.models.Word2Vec(sent_tokens, size=20,workers=6)
-        model.save('srcname.wtvmodel')
+        model.save('results/{}.wtvmodel'.format(srcname))
 
