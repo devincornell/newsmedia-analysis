808d71ea4f168b6c7570225174ed61c928660032
Devin Cornell <devinj.cornell@gmail.com>
2017-02-25 18:13:02 -0800

    Added feature to save WordFreq to pickle file and remove non-unicode characters from tokens.

--- build_models.py
+++ build_models.py
@@ -4,12 +4,17 @@ import json
 import nltk
 import nltk.corpus
 from nltk.stem.wordnet import WordNetLemmatizer
+import pickle
 
 def remove_specialchars(tstr):
     return tstr\
         .replace(u'\\','')\
         .replace(u"'",'')\
-        .replace(u'"','')
+        .replace(u'"','')\
+        .replace(u'--','')\
+        .replace(u'\u201c','')\
+        .replace(u'\u2019','')\
+        .replace(u'\xe2\x80\x94','') # didn't work :(
 
 def get_source_data(fname,verbose=False):
     if verbose: print('Loading data file {}.'.format(fname))
@@ -29,7 +34,7 @@ def get_source_data(fname,verbose=False):
 if __name__ == "__main__":
 
     # script params
-    outfile = "results/model.idk"
+    results_folder = 'results/'
 
     sources = {
         'breitbart': 'Data/scraped_articles_breitbart.json',
@@ -53,6 +58,9 @@ if __name__ == "__main__":
         # break each sentence into a list of lower case words without the '.' character
         src_sent = [list(map(lambda x: x.lower(),sent.replace('.','').split())) for sent in src_sent]
 
+        # apply ascii encodings (by ommitting non-ascii chars)
+        src_sent = [list(map(lambda x: x.encode('ascii',errors='ignore').decode(), sent)) for sent in src_sent]
+        
         # remove stopwords from sentences
         src_sent = [list(filter(lambda x: x not in stopwords, sent)) for sent in src_sent]
 
@@ -60,9 +68,18 @@ if __name__ == "__main__":
         lmtzr = WordNetLemmatizer()
         src_sent = [list(map(lambda x:lmtzr.lemmatize(x),sent)) for sent in src_sent]
 
+        # calculate frequency information for each word
+        freq_dist = nltk.FreqDist([w for s in src_sent for w in s])
+
         # train model on all sentences from source
         print('{} sentences for {}.'.format(len(src_sent), srcname))
         print("Training model on {}".format(srcname))
         model = gensim.models.Word2Vec(src_sent, size=20,workers=6)
-        model.save('results/{}.wtvmodel'.format(srcname))
+
+        # save model and word frequency count
+        print('Saving {}{}.wtvmodel and {}.pickle'.format(results_folder,srcname, srcname+'_wordfreq'))
+        model.save('{}{}.wtvmodel'.format(results_folder,srcname))
+        with open(results_folder + srcname + '_wordfreq.pickle','wb') as f:
+            pickle.dump(freq_dist, f)
+        print()
 
