6ce78195c3433ee76c7542dd385194ae96ae87d3
Devin Cornell <devinj.cornell@gmail.com>
2017-03-13 18:29:29 -0700

    Moved sparsification to semanticnetwork.py

--- semanticnetwork.py
+++ semanticnetwork.py
@@ -4,9 +4,12 @@ import gensim.models
 import itertools
 import numpy as np
 from multiprocessing import Pool
-from functools import partial
 import time
+import functools
 
+
+
+##### Semantic Network Construction #####
 
 def get_cosdist(u_vec,v_vec):
     #u_vec, v_vec = e[0],e[1]
@@ -118,9 +121,87 @@ def build_semanticnetwork(
     return G
     
 
+
+##### Sparsify Functions #####
+
+import operator as op
+def ncr(n, r):
+    # stole from http://stackoverflow.com/questions/4941753/is-there-a-math-ncr-function-in-python
+    r = min(r, n-r)
+    if r == 0: return 1
+    numer = functools.reduce(op.mul, range(n, n-r, -1))
+    denom = functools.reduce(op.mul, range(1, r+1))
+    return numer//denom
+
+def pvalf(edat):
+    return pval(*edat)
+
+def pval(e,weight,ku,kv,T):
+    ''' given by equations 1-3 in "Unwinding the Hairball Graph"
+    weight: weight of edge in question
+    ku: sum(weights of edges on u)
+    kv: sum(weights of edges on v)
+    T: sum of all edge weights in the graph
+    '''
+    max_num = 1e20
+    w = int(weight);
+    max_m = w + 10
+    p = ku*kv/(2*T**2);
+    T = int(T);
+
+    #print(ncr(T,w))
+    pval = 0
+    for m in range(w,max_m):
+        choose = ncr(T,m)
+        if choose > max_num: choose = max_num
+        pval += choose * (p**m) * (1-p)**(T-m)
+
+    return (e,pval)
+
+def deg(n,G,attr='weight'):
+    # calculate degree of node n for weighted graph
+    hood = G.neighbors(n)
+    return sum([G.edge[n][v][attr] for v in hood])
+
+def sparsify_graph(
+    G=None,
+    keep_fraction=0.5,
+    weight_attr='weight',
+    pval_attr='p-val',
+    processes=16,
+    verbose=False,
+    ):
+
+
+    if verbose: print('Compiling pool data..')
+    T = sum(nx.get_edge_attributes(G,weight_attr).values())
+    pvals = dict()
+    degrees = {u:deg(u,G) for u in G.nodes()}
+
+    edata = (((u,v),G.edge[u][v][weight_attr],degrees[u],degrees[v],T) for (u,v) in G.edges())
+    
+    if verbose: print('Starting sparsification..')
+    p = Pool(processes) # will never use that many
+    pvals = p.map(pvalf,edata)
+    if verbose: print('Sparsification finished.')
+
+    nx.set_edge_attributes(G,pval_attr,{x[0]:x[1] for x in pvals})
+
+    # remove (based on p-value) n edges where n = numedges*(1-edge_cutoff)
+    if verbose: print('Removing crappy edges..')
+    edges = G.edges(data=True)
+    sedges = sorted(edges,key=lambda x:x[2][pval_attr])
+    remove_edges = [(x[0],x[1]) for x in sedges[int(len(edges)*keep_fraction):]]
+    G.remove_edges_from(remove_edges)
+    num_edges = len(G.edges())
+    if verbose: print('{}% of edges retained: {} remain.'.format(int(num_edges/len(edges)*100),num_edges))
+    
+    return G
+
+
 if __name__ == '__main__':
 
-    model = gensim.models.Word2Vec([['a','b','c'],['c','a','lol','haha'],['this','sucks','a','c']],size=3,min_count=1)
+    #model = gensim.models.Word2Vec([['a','b','c'],['c','a','lol','haha'],['this','sucks','a','c']],size=3,min_count=1)
     model = gensim.models.Word2Vec.load('results/cnn.wtvmodel')
 
     usenodes = list(model.vocab)[:1000]
@@ -130,9 +211,26 @@ if __name__ == '__main__':
         'usenodes':usenodes, 
         'verbose':True, 
         'nodeattrs': { 
-            'eigcent': nx.eigenvector_centrality,
+            'eigcent': lambda x: nx.eigenvector_centrality(x,1000,tol=1e-4),
             },
+        'edgeattrs': {
+            'weight': calc_edgeweights,
+            }, 
         }
     G = build_semanticnetwork(**settings)
+    
+    print('Now sparsifying..')
+
+    settings = {
+        'G': G,
+        'keep_fraction': 0.01,
+        'weight_attr': 'weight',
+        'pval_attr': 'p-val',
+        'processes': 16,
+        'verbose':True,
+        }
+    G = sparsify_graph(**settings)
+
+
     print('Saving file..')
-    nx.write_gexf(G,'results/test.gexf')
+    nx.write_gexf(G,'results/test_sparse.gexf')
