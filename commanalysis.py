8400ae876e8a24ffde7925a9b23b75e1fb891015
Devin Cornell <devinj.cornell@gmail.com>
2017-03-21 16:59:05 -0700

    Removed unused functions from comm analysis.

--- commanalysis.py
+++ commanalysis.py
@@ -3,45 +3,25 @@ import networkx as nx
 import semanticnetwork as sn
 import math
 import pickle
-
-mftnames = {
-    1: 'HarmVirtue',
-    2: 'HarmVice',
-    3: 'FairnessVirtue',
-    4: 'FairnessVice',
-    5: 'IngroupVirtue',
-    6: 'IngroupVice',
-    7: 'AuthorityVirtue',
-    8: 'AuthorityVice',
-    9: 'PurityVirtue',
-    10: 'PurityVice',
-    11: 'MoralityGeneral',
-}
-
-
-def getmftnodes(nodeattr):
-    nodedict = {x:list() for x in mftnames.keys()} # moral foundation -> list(nodes)
-    for m in mftnames.keys():
-        for n,v in nodeattr.items():
-            if str(m) in v.split(','):
-                nodedict[m].append(n)
-    return nodedict
-
+import community
 
 if __name__ == '__main__':
+    # this script uses the community library
+    # http://perso.crans.org/aynaud/communities/api.html
     folder = 'results/'
     extension = '.gexf'
     srcnames = ['breitbart', 'cbsnews', 'cnn', 'foxnews', 'nytimes']
 
-    mftscores = {s:{m:0 for m in mftnames.keys()} for s in srcnames}
+    partitions = dict()
     for src in srcnames:
         print('Reading', src, 'graph.')
         G = nx.read_gexf(folder + src + extension)
-        print('Calculating clusters..')
         
-        #first compute the best partition
-        partitions = community.best_partition(G)
+        print('Calculating clusters..')
+        partitions[src] = community.best_partition(G)
+        print()
 
+    print('Saving files..')
     with open(folder + 'cluster_partitions.pickle', 'wb') as f:
         pickle.dump(partitions,f)
 
