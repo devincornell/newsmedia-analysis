e22fbab4c6f535cba0bde390cd8774c3eafbf223
Devin Cornell <devinj.cornell@gmail.com>
2017-03-21 11:39:59 -0700

    Set to output difference graph with sources as nodes - distances are l2norms between mft vectors (each category is a dimension).

---
+++ mftpathanalysis.py
@@ -0,0 +1,84 @@
+
+import networkx as nx
+import semanticnetwork as sn
+import math
+
+mftnames = {
+    1: 'HarmVirtue',
+    2: 'HarmVice',
+    3: 'FairnessVirtue',
+    4: 'FairnessVice',
+    5: 'IngroupVirtue',
+    6: 'IngroupVice',
+    7: 'AuthorityVirtue',
+    8: 'AuthorityVice',
+    9: 'PurityVirtue',
+    10: 'PurityVice',
+    11: 'MoralityGeneral',
+}
+
+
+def getmftnodes(nodeattr):
+    nodedict = {x:list() for x in mftnames.keys()} # moral foundation -> list(nodes)
+    for m in mftnames.keys():
+        for n,v in nodeattr.items():
+            if str(m) in v.split(','):
+                nodedict[m].append(n)
+    return nodedict
+
+def vdiff(uv,vv):
+    return l2norm([u-v for u in uv for v in vv])
+
+
+def l2norm(listvec):
+    norm = 0
+    for v in listvec:
+        norm += v**2
+
+    return math.sqrt(norm)
+
+if __name__ == '__main__':
+    folder = 'results/'
+    extension = '_sparse.gexf'
+    srcnames = ['breitbart', 'cbsnews', 'cnn', 'foxnews', 'nytimes']
+    testword = 'trump'
+
+    mftscores = {s:{m:0 for m in mftnames.keys()} for s in srcnames}
+    print('Using testword', testword)
+    for src in srcnames:
+        print('Reading', src, 'graph.')
+        G = nx.read_gexf(folder + src + extension)
+        print('Calculating av path len for each mft category..')
+        mftattr = nx.get_node_attributes(G, 'mft')
+        mftmap = getmftnodes(mftattr)
+        for moral,nodes in mftmap.items():
+            #print(mftnames[moral], 'has', len(nodes), 'nodes.')
+            for n in nodes:
+                pl = 0
+                pl = nx.shortest_path_length(G,testword,n,weight='dist')
+                try:
+                    pl = nx.shortest_path_length(G,testword,n,weight='dist')
+                except:
+                    pass
+                mftscores[src][moral] += pl
+            if len(nodes) > 0:
+                mftscores[src][moral] /= len(nodes) # av path len
+
+        for moral,apl in mftscores[src].items():
+            print(mftnames[moral], 'had apl', apl)
+
+        print()
+
+    # make distance graph between sources based on mft vectors
+    # each moral is a dimension in that vector space
+    G = nx.Graph()
+    G.add_nodes_from(srcnames)
+    for u in G.nodes():
+        for v in G.nodes():
+            if u != v:
+                dist = vdiff(mftscores[u].values(), mftscores[v].values())
+                weight = 1/dist
+                G.add_edge(u,v,diff=dist,weight=weight)
+    
+    nx.write_gexf(G, 'results/srcdiffgraph.gexf')
+
