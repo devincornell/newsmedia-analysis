fbd5af7c8c12f15fe96f278bad2019d6cd5e67c6
Devin Cornell <devinj.cornell@gmail.com>
2017-10-20 09:15:13 -0700

    Made some changes.

--- buildnetworks.py
+++ buildnetworks.py
@@ -74,10 +74,10 @@ if __name__ == "__main__":
     # reduction/sparsification settings
     drop_nodes = False
     num_nodes_retained = 30 # number of most central nodes to keep
-    sparsify_edges = False
+    sparsify_edges = True
     sparsify_retain_ratio = 0.3 # percentage of edges to keep
     drop_edges = False
-    fraction_edges_retained = 0.5 # percentage of edges to keep (after sparsifying, if nessecary)
+    fraction_edges_retained = 0.3 # percentage of edges to keep (after sparsifying, if nessecary)
  
 
     ## CODE STARTS
@@ -143,7 +143,7 @@ if __name__ == "__main__":
             'usenodes': nodeset, 
             'verbose': True,
             'nodeattrs': {
-                'eigcent': lambda xG: nx.eigenvector_centrality(xG,10000,tol=1e-3),
+                #'eigcent': lambda xG: nx.eigenvector_centrality(xG,1000000,tol=1e-2),
                 'wordfreq': lambda xG: {x:wf[x] for x in xG.nodes()},
                 'mft': lambda xG: mftmatches,
                 },
@@ -173,5 +173,5 @@ if __name__ == "__main__":
             G = sn.drop_edges(G,'weight', fraction=fraction_edges_retained, keep_largest=True, verbose=True)
 
         print('Writing file..')
-        nx.write_gexf(G, results_folder + src + '_sparse.gexf')
+        nx.write_gexf(G, results_folder + src + '_sparsified.gexf')
         print()
